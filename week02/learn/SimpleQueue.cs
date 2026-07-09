6659f55ead396e3d0a993838e04bdcd8a57014bb
owenread <oread01p@gmail.com>
2026-07-09 02:23:58 -0600

    FIxed w2 learning activity issues and compared my work

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -1,5 +1,9 @@
-﻿public class SimpleQueue {
-    public static void Run() {
+﻿using System.Security.Cryptography;
+
+public class SimpleQueue
+{
+    public static void Run()
+    {
         // Test Cases
 
         // Test 1
@@ -10,7 +14,7 @@
         queue.Enqueue(100);
         var value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found:
+        // Defect(s) Found: Dequeue method was starting at index 1 not 0. 
 
         Console.WriteLine("------------");
 
@@ -28,7 +32,7 @@
         Console.WriteLine(value);
         value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found: 
+        // Defect(s) Found: It was inserting the value instead of adding the value to the back of the queue. 
 
         Console.WriteLine("------------");
 
@@ -37,11 +41,13 @@
         // Expected Result: An exception should be raised
         Console.WriteLine("Test 3");
         queue = new SimpleQueue();
-        try {
+        try
+        {
             queue.Dequeue();
             Console.WriteLine("Oops ... This shouldn't have worked.");
         }
-        catch (IndexOutOfRangeException) {
+        catch (IndexOutOfRangeException)
+        {
             Console.WriteLine("I got the exception as expected.");
         }
         // Defect(s) Found: 
@@ -53,8 +59,9 @@
     /// Enqueue the value provided into the queue
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
-    private void Enqueue(int value) {
-        _queue.Insert(0, value);
+    private void Enqueue(int value)
+    {
+        _queue.Add(value); // pretty sure you cannot insert with a queue. That would be cutting in line
     }
 
     /// <summary>
@@ -62,12 +69,13 @@
     /// </summary>
     /// <exception cref="IndexOutOfRangeException">If queue is empty</exception>
     /// <returns>First integer in the queue</returns>
-    private int Dequeue() {
+    private int Dequeue()
+    {
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0]; // changed 1 to 0 since this code was indexing incorrectly. 
+        _queue.RemoveAt(0);
         return value;
     }
 }
